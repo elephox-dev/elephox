06c8e949d79b2fee722845c0e85f8951fcd9ac85
Ricardo Boss <contact@ricardoboss.de>
2021-11-17 02:26:36 +0100

    Added more handlers and implemented changes from framework

--- src/App.php
+++ src/App.php
@@ -3,8 +3,12 @@ declare(strict_types=1);
 
 namespace App;
 
-use Elephox\Core\Handler\Contract\RequestContext;
-use Elephox\Core\Handler\RequestHandler;
+use Elephox\Core\Context\Contract\CommandLineContext;
+use Elephox\Core\Context\Contract\ExceptionContext;
+use Elephox\Core\Context\Contract\RequestContext;
+use Elephox\Core\Handler\Attribute\CommandHandler;
+use Elephox\Core\Handler\Attribute\ExceptionHandler;
+use Elephox\Core\Handler\Attribute\RequestHandler;
 use Elephox\Http\Contract;
 use Elephox\Http\RequestMethod;
 use Elephox\Http\Response;
@@ -22,9 +26,35 @@ class App
         );
     }
 
-    #[RequestHandler('/login', RequestMethod::POST)]
+    #[RequestHandler('login', RequestMethod::POST)]
     public function handleLogin(RequestContext $context): Contract\Response
     {
+        return Response::withJson([]);
+    }
 
+    #[RequestHandler('{anything}')]
+    public function catchAll(RequestContext $context): Contract\Response
+    {
+        return Response::withJson([
+            'message' => 'You sent a request to an invalid endpoint: ' . $context->getRequest()->getUrl(),
+        ]);
+    }
+
+    #[CommandHandler]
+    public function commandLineHandler(CommandLineContext $context): void
+    {
+        echo "You successfully invoked your Elephox app from command line!";
+    }
+
+    #[ExceptionHandler]
+    public function globalExceptionHandler(ExceptionContext $context): void
+    {
+        echo <<<TEXT
+Uh-oh! This shouldn't have happened. An exception ocurred at {$context->getException()->getFile()}:{$context->getException()->getLine()}
+
+> {$context->getException()->getMessage()}
+
+{$context->getException()->getTraceAsString()}
+TEXT;
     }
 }
