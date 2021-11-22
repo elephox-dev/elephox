d9d82d806a374f39462bc711394075718bdf0646
Ricaro Boss <contact@ricardoboss.de>
2021-11-22 16:17:08 +0100

    Added vlucas/phpdotenv and added more command line examples

--- src/App.php
+++ src/App.php
@@ -5,6 +5,7 @@ namespace App;
 
 use App\Models\User;
 use App\Repositories\UserRepository;
+use Dotenv\Dotenv;
 use Elephox\Core\Context\Contract\CommandLineContext;
 use Elephox\Core\Context\Contract\ExceptionContext;
 use Elephox\Core\Context\Contract\RequestContext;
@@ -25,134 +26,144 @@ use Whoops\Run as WhoopsRunner;
 
 class App implements AppContract
 {
-    public function __construct(Container $container)
-    {
-        $container->register(Storage::class, static function () {
-            $dsn = Url::fromString($_ENV['DB_DSN']);
-            $connection = mysqli_connect($dsn->getHost(), $dsn->getUsername(), $dsn->getPassword(), trim($dsn->getPath(), '/'));
+	public function __construct(Container $container)
+	{
+		Dotenv::createImmutable(dirname(__DIR__))->load();
 
-            return new MysqlStorage($connection);
-        });
+		$container->register(Storage::class, static function () {
+			$dsn = Url::fromString($_ENV['DB_DSN']);
+			$connection = mysqli_connect($dsn->getHost(), $dsn->getUsername(), $dsn->getPassword(), trim($dsn->getPath(), '/'));
 
-        $container->register(UserRepository::class);
-        $container->register(WhoopsRunner::class);
-    }
+			return new MysqlStorage($connection);
+		});
 
-    #[Get('/')]
-    public function handleIndex(): Contract\Response
-    {
-        return Response::withJson([
-            'message' => 'Hello, World! Send a POST request to /info to get more info.',
-            'ts' => microtime(true) - ELEPHOX_START,
-        ]);
-    }
+		$container->register(UserRepository::class);
+		$container->register(WhoopsRunner::class);
+	}
 
-    #[Post('login')]
-    public function login(RequestContext $context, UserRepository $userRepository): Contract\Response
-    {
-        $json = $context->getRequest()->getJson();
-        $username = $json['username'];
-        if (!$username) {
-            return Response::withJson([
-                'message' => 'Username is required.',
-                'ts' => microtime(true) - ELEPHOX_START,
-            ]);
-        }
+	#[Get('/')]
+	public function handleIndex(): Contract\Response
+	{
+		return Response::withJson([
+			'message' => 'Hello, World! Send a POST request to /register to create an account or to /login to log in.',
+			'ts' => microtime(true) - ELEPHOX_START,
+		]);
+	}
 
-        /** @var \App\Models\User|null $user */
-        $user = $userRepository->findBy('username', $username);
-        if ($user === null) {
-            return Response::withJson([
-                'message' => 'User not found',
-                'ts' => microtime(true) - ELEPHOX_START,
-            ]);
-        }
+	#[Post('login')]
+	public function login(RequestContext $context, UserRepository $userRepository): Contract\Response
+	{
+		$json = $context->getRequest()->getJson();
+		$username = $json['username'];
+		if (!$username) {
+			return Response::withJson([
+				'message' => 'Username is required.',
+				'ts' => microtime(true) - ELEPHOX_START,
+			]);
+		}
 
-        if (!array_key_exists('password', $json) || !$user->verifyPassword($json['password'])) {
-            return Response::withJson([
-                'message' => 'Invalid password',
-                'ts' => microtime(true) - ELEPHOX_START,
-            ]);
-        }
+		/** @var User|null $user */
+		$user = $userRepository->findBy('username', $username);
+		if ($user === null) {
+			return Response::withJson([
+				'message' => 'User not found',
+				'ts' => microtime(true) - ELEPHOX_START,
+			]);
+		}
 
-        return Response::withJson([
-            'message' => "Welcome back, {$user->getUsername()}!",
-            'ts' => microtime(true) - ELEPHOX_START,
-        ]);
-    }
+		if (!array_key_exists('password', $json) || !$user->verifyPassword($json['password'])) {
+			return Response::withJson([
+				'message' => 'Invalid password',
+				'ts' => microtime(true) - ELEPHOX_START,
+			]);
+		}
 
-    #[Post('register')]
-    public function register(RequestContext $context, UserRepository $userRepository): Contract\Response
-    {
-        $json = $context->getRequest()->getJson();
-        $username = $json['username'];
-        if (!$username) {
-            return Response::withJson([
-                'message' => 'Username is required.',
-                'ts' => microtime(true) - ELEPHOX_START,
-            ]);
-        }
+		return Response::withJson([
+			'message' => "Welcome back, {$user->getUsername()}!",
+			'ts' => microtime(true) - ELEPHOX_START,
+		]);
+	}
 
-        /** @var \App\Models\User|null $user */
-        $user = $userRepository->findBy('username', $username);
-        if ($user !== null) {
-            return Response::withJson([
-                'message' => 'Username already exists',
-                'ts' => microtime(true) - ELEPHOX_START,
-            ]);
-        }
+	#[Post('register')]
+	public function register(RequestContext $context, UserRepository $userRepository): Contract\Response
+	{
+		$json = $context->getRequest()->getJson();
+		$username = $json['username'];
+		if (!$username) {
+			return Response::withJson([
+				'message' => 'Username is required.',
+				'ts' => microtime(true) - ELEPHOX_START,
+			]);
+		}
 
-        if (!array_key_exists('password', $json)) {
-            return Response::withJson([
-                'message' => 'Password is required.',
-                'ts' => microtime(true) - ELEPHOX_START,
-            ]);
-        }
+		/** @var User|null $user */
+		$user = $userRepository->findBy('username', $username);
+		if ($user !== null) {
+			return Response::withJson([
+				'message' => 'Username already exists',
+				'ts' => microtime(true) - ELEPHOX_START,
+			]);
+		}
 
-        if (!array_key_exists('email', $json)) {
-            return Response::withJson([
-                'message' => 'Email is required.',
-                'ts' => microtime(true) - ELEPHOX_START,
-            ]);
-        }
+		if (!array_key_exists('password', $json)) {
+			return Response::withJson([
+				'message' => 'Password is required.',
+				'ts' => microtime(true) - ELEPHOX_START,
+			]);
+		}
 
-        $user = User::from($username, $json['password'], $json['email']);
-        $userRepository->add($user);
+		if (!array_key_exists('email', $json)) {
+			return Response::withJson([
+				'message' => 'Email is required.',
+				'ts' => microtime(true) - ELEPHOX_START,
+			]);
+		}
 
-        return Response::withJson([
-            'message' => "Successfully registered, {$user->getUsername()}!",
-            'ts' => microtime(true) - ELEPHOX_START,
-        ]);
-    }
+		$user = User::from($username, $json['password'], $json['email']);
+		$userRepository->add($user);
 
-    #[Post('info')]
-    public function info(): Contract\Response
-    {
-        return Response::withJson([
-            'message' => "You successfully POSTed! Next, try to comment out the 'catchAll' handler and see your exception handler at work.",
-            'ts' => microtime(true) - ELEPHOX_START,
-        ]);
-    }
+		return Response::withJson([
+			'message' => "Successfully registered, {$user->getUsername()}!",
+			'ts' => microtime(true) - ELEPHOX_START,
+		]);
+	}
 
-    #[Any('(?<anything>.*)')]
-    public function catchAll(RequestContext $context, string $anything): Contract\Response
-    {
-        return Response::withJson([
-            'message' => "You sent a request to an invalid endpoint: $anything Perhaps your request method ({$context->getRequest()->getMethod()->getValue()}) was invalid?",
-            'ts' => microtime(true) - ELEPHOX_START,
-        ]);
-    }
+	#[Any('(?<anything>.*)')]
+	public function catchAll(RequestContext $context, string $anything): Contract\Response
+	{
+		return Response::withJson([
+			'message' => "You sent a request to an invalid endpoint: $anything Perhaps your request method ({$context->getRequest()->getMethod()->getValue()}) was invalid?",
+			'tip' => "You can comment out the App::catchAll method to see your exception handler deal with an unmatched request.",
+			'ts' => microtime(true) - ELEPHOX_START,
+		]);
+	}
 
-    #[CommandHandler]
-    public function commandLineHandler(CommandLineContext $context): void
-    {
-        echo "You successfully invoked your Elephox app from command line!";
-    }
+	#[CommandHandler("/info\s?(?<args>.*)/")]
+	public function infoCommand(): void
+	{
+		echo "Your app runs with the Elephox Framework version " . ELEPHOX_VERSION;
+	}
 
-    #[ExceptionHandler]
-    public function globalExceptionHandler(ExceptionContext $context, WhoopsRunner $whoops): void
-    {
-        $whoops->pushHandler(new PrettyPageHandler);
-        $whoops->handleException($context->getException());
-    }
+	#[CommandHandler]
+	public function commandLineHandler(CommandLineContext $context): void
+	{
+		echo "You successfully invoked your Elephox app from command line!" . PHP_EOL;
+		echo "\tCommand: " . $context->getCommand() . PHP_EOL;
+		echo "Try running it with the 'info' command" . PHP_EOL;
+	}
+
+	#[ExceptionHandler]
+	public function globalExceptionHandler(ExceptionContext $context, ?CommandLineContext $commandLineContext, WhoopsRunner $whoops): void
+	{
+		// handle command line exceptions
+		if ($commandLineContext !== null) {
+			echo PHP_EOL . "Command not found: " . $commandLineContext->getCommand() . PHP_EOL;
+
+			exit(1);
+		}
+
+		// fallback handler for all other exceptions
+		$whoops->pushHandler(new PrettyPageHandler);
+		$whoops->handleException($context->getException());
+	}
 }
