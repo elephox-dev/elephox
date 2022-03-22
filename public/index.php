9216dce60d10cb4c87cddc6c888ee6e26003acc5
Ricardo Boss <contact@ricardoboss.de>
2022-03-22 02:28:48 +0100

    WIP

--- public/index.php
+++ public/index.php
@@ -3,14 +3,14 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/bootstrap.php';
 
-use Elephox\Configuration\Json\JsonFileConfigurationSource;
+use Elephox\Host\Middleware\DefaultExceptionHandler;
 use Elephox\Host\WebApplication;
 
 $builder = WebApplication::createBuilder();
-$app = $builder->build();
 
-if ($app->environment->isDevelopment()) {
-	$app->services->addWhoops();
-}
+$builder->services->addWhoops();
 
-$app->run();
+$builder->pipeline->addRouting();
+$builder->pipeline->push(new DefaultExceptionHandler());
+
+$builder->build()->run();
