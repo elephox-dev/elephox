86daee1996fc7ccd9d7e543dd2237d9f1baa58f7
Ricardo Boss <contact@ricardoboss.de>
2022-03-24 18:58:41 +0100

    WIP

--- src/Routes/RequestHandlers.php
+++ src/Routes/RequestHandlers.php
@@ -4,34 +4,27 @@ namespace App\Routes;
 
 use Elephox\Core\Handler\Attribute\Http\Any;
 use Elephox\Core\Handler\Attribute\Http\Get;
+use Elephox\Http\Contract\Response as ResponseContract;
 use Elephox\Http\Response;
 use Elephox\Http\ResponseCode;
 use Elephox\Stream\StringStream;
 use RuntimeException;
 
+#[Controller("/")]
 class RequestHandlers
 {
 	#[Get]
-	public function handleIndex(): Response
+	public function index(): ResponseContract
 	{
 		return Response::build()
 			->responseCode(ResponseCode::OK)
-			->body(new StringStream('Hello world!'))
+			->htmlBody('<h1>Hello world!</h1>')
 			->get();
 	}
 
-	#[Get('throw')]
-	public function testThrow(): never
+	#[Get]
+	public function throw(): never
 	{
 		throw new RuntimeException('Test exception');
-	}
-
-	#[Any('(?<anything>.*)')]
-	public function catchAll(string $anything): Response
-	{
-		return Response::build()
-			->responseCode(ResponseCode::NotFound)
-			->body(new StringStream("Requested resource not found: $anything"))
-			->get();
 	}
 }
