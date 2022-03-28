6a8686062dc274bffa9f39860a582a6ae12a50b2
Ricardo Boss <contact@ricardoboss.de>
2022-03-29 01:13:48 +0200

    WIP

--- public/index.php
+++ public/index.php
@@ -3,8 +3,17 @@ declare(strict_types=1);
 
 require_once dirname(__DIR__) . '/bootstrap.php';
 
-$serialized = file_get_contents('cache/web.phpo');
-/** @var \Elephox\Web\WebApplication $app */
-$app = unserialize($serialized);
+if (!file_exists(dirname(__DIR__) . '/cache/web.phpo')) {
+	/** @var \Elephox\Web\WebApplication $app */
+	$app = require dirname(__DIR__) . '/buildApp.php';
+	$serialized = serialize($app);
+
+	file_put_contents(dirname(__DIR__) . '/cache/web.phpo', $serialized);
+} else {
+	$serialized = file_get_contents(dirname(__DIR__) . '/cache/web.phpo');
+
+	/** @var \Elephox\Web\WebApplication $app */
+	$app = unserialize($serialized);
+}
 
 $app->run();
